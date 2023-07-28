17484c104ec07af394ed5d8fec513047d3a4daa9
Atinder Kumar <111070211+atinder11@users.noreply.github.com>
2023-07-28 11:04:38 +0530

    Add the sum.cpp file
    adding the cpp file

---
+++ sum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    int sum=0;
+    cout<<"ENTER THE TWO NUMBER :";
+    cin>>a>>b;
+    sum=a+b;
+    cout<<endl;
+
+    cout<<"Sum :"<<sum;
+
+
+
+
+
+
+return 0;
+}
